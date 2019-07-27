168ea07194ac1cebe3eaf10d8be350abc5443639
Gupta <pgupta@internal.tavisca.com>
2019-07-27 10:16:26 +0530

    Refactored the solution

---
+++ MarsRover/StepForwardSouthWest.cs
@@ -0,0 +1,18 @@
+﻿namespace MarsRover
+{
+    public class StepForwardSouthWest : IStepForward
+    {
+        public PositionVector StepForward(PositionVector currentPositionVector, Coordinate maxCoordinateToRove)
+        {
+            PositionVector updatedPositionVector = currentPositionVector;
+
+                updatedPositionVector.Coordinate.X = currentPositionVector.Coordinate.X - 1;
+                updatedPositionVector.Coordinate.Y = currentPositionVector.Coordinate.Y - 1;
+
+            if (updatedPositionVector.Coordinate.CheckBounds(maxCoordinateToRove) == false)
+                return updatedPositionVector;
+            return currentPositionVector;
+
+        }
+    }
+}
